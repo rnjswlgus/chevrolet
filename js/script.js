ea5da09f7581f40dbb3700e439503aab38857bcc
권지현 <89637757+rnjswlgus@users.noreply.github.com>
2021-11-19 12:11:57 +0900

    쉐보레 1차 클론코딩 업로드

---
+++ js/script.js
@@ -0,0 +1,50 @@
+window.onload = function() {
+    $(document).ready(function() {
+
+        //모달창
+        let modal = $('.modal');
+        let modal_cont = $('.modal-cont');
+        let modal_close = $('.modal-close');
+
+        $('.modal-close').click(function() {
+            modal.fadeOut();
+        });
+
+        $('.modal').click(function() {
+            modal.fadeOut();
+        });
+
+        $('.modal-cont').click(function(event) {
+            event.stopPropagation();
+        });
+
+
+
+
+
+
+        //visual-swiper
+        var sw_visual = new Swiper('.sw-visual', {
+            slidesPerView: 1,
+            spaceBetween: 0,
+            loop: true,
+            autoplay: {
+                delay: 2100,
+                disableOnInteraction: true,
+            },
+            speed: 800,
+            navigator: {
+                prevEl: 'swiper-button-prev',
+                nextEl: 'swiper-button-next',
+            },
+        });
+
+        $('.sw-visual').mouseenter(function() {
+            sw_visul.autoplay.stop();
+        });
+        $('.sw-visual').mousewheel(function() {
+            sw_visul.autoplay.start();
+        });
+
+    });
+};
